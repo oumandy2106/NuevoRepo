1467dfe66475a7116f054c5768d0b1dc8aa9a4c5
oumandy2106 <20243ds039@utez.edu.mx>
2025-06-13 08:46:26 -0600

    Añadi comentario

--- U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
+++ U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        //ayudame diosito con esta vida
     }
 
     public static void main(String[] args) {
