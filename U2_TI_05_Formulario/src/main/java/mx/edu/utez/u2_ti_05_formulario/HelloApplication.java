a6edc717505ca011d72a9c9554b8f0a90072fbf7
oumandy2106 <20243ds039@utez.edu.mx>
2025-06-13 08:48:37 -0600

    Añadi botones

--- U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
+++ U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
@@ -11,11 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-        //ayudame diosito con esta vida
+    //Labels y botones
+        Label label = new Label("Chiste bien gracioso");
+        Button button = new Button("Chiste");
+        Button button1 = new Button("Remate");
+
+
     }
 
     public static void main(String[] args) {
