d9e61bbf81863363e0daf3ab91e558238b7b3ac2
oumandy2106 <20243ds039@utez.edu.mx>
2025-06-13 09:17:53 -0600

    Añadi los labels y los textfields

--- U2_TI_06_Trabajo/src/main/java/mx/edu/utez/u2_ti_06_trabajo/HelloApplication.java
+++ U2_TI_06_Trabajo/src/main/java/mx/edu/utez/u2_ti_06_trabajo/HelloApplication.java
@@ -1,20 +1,47 @@
 package mx.edu.utez.u2_ti_06_trabajo;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        Label lblNombre = new Label("Nombre: ");
+        TextField tfNombre = new TextField();
+
+        Label lblApellido = new Label("Apellido: ");
+        TextField tfApellido = new TextField();
+
+        Label lblEdad = new Label("Edad ");
+        TextField tfEdad = new TextField();
+
+        Label lblGrupo = new Label("Grupos: ");
+        ObservableList<String> grupos = FXCollections.observableArrayList("Estudiante", "Profesor", "admin");
+        ComboBox<String> cbGrupo = new ComboBox<>(grupos);
+
+        Button btnEnviar = new Button("Enviar");
+        Label resultado = new Label();
+
+        VBox vbox = new VBox(20,label, button, button1);
+        vbox.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(vbox, 500, 240);
+
+
+
+
+
     }
 
     public static void main(String[] args) {
